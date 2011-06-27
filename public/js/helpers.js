42a7e4009f8abb0d2c9ed50f61434a47f3b1e191
Esa-Matti Suuronen <esa-matti@suuronen.org>
2011-06-28 02:00:44 +0300

    fix missing consoles

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -1,3 +1,10 @@
+
+if (!window.console || !window.console.log) {
+    window.console = {
+      log:  function() {}
+    };
+}
+
 $(function(){
 
   $(".here").text(window.location.href).attr("href",window.location.href);
